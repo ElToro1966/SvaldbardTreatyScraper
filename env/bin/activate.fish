a7a6d928f10f9731e24a84553f70a5326d4cadd6
ElToro1966 <mail@ericeikrem.com>
2018-09-11 10:01:51 +0200

    Simplify requirements.txt-file.

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/eric/Git/SvaldbardTreatyScraper/sts"
+set -gx VIRTUAL_ENV "/home/eric/Git/SvaldbardTreatyScraper/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- sts/sts/spiders/regjeringen_no.py
+++ sts/sts/spiders/regjeringen_no.py
@@ -3,10 +3,17 @@ import scrapy
 
 
 class RegjeringenNoSpider(scrapy.Spider):
+    '''A spider to crawl the Norwegian Government's pages covering the Treaty of Svalbard'''
     name = "regjeringen_no"
     count = 0
+    #rules = (
+    #    Rule(LinkExtractor(allow='regjeringen\.no\/no\/[a-z_/]+$'),
+    #        'parse_category', follow=True,
+    #    ),
+    #)
 
     def start_requests(self):
+        '''Returns an iterable of Requests which the Spider will begin to crawl from'''
         urls = [
             'https://www.regjeringen.no/no/sok/id86008/?sortby=1&term=Svalbardtraktaten',
             'https://www.regjeringen.no/no/sok/id86008/?sortby=1&term=Svalbardtraktaten&page=2',
@@ -20,8 +27,17 @@ class RegjeringenNoSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        '''Handles the response downloaded for each of the requests made'''
+        self._save_response_to_html(response)
+        for article in response.css("ul.listing"):
+            title = article.css("h2.title a::text").extract_first()
+
+
+    def _save_response_to_html(self, response):        
         self.count = self.count + 1
         filename = 'response/regjeringen_no-%s.html' % str(self.count)
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
+
