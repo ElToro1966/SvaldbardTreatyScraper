65e1e512d0071d7c6e9d4706988bbaa06476e806
ElToro1966 <mail@ericeikrem.com>
2024-12-11 03:53:46 -0500

    Update dependency

--- sts/spiders/regeringen_dk.py
+++ sts/spiders/regeringen_dk.py
@@ -1,21 +1,24 @@
+from abc import ABC
+
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-class RegeringenDkSpider(CrawlSpider):
-    '''Spider crawling the Danish Government's news pages'''
+
+class RegeringenDkSpider(CrawlSpider, ABC):
+    """Spider crawling the Danish Government's news pages"""
     name = 'regeringen_dk'
     allowed_domains = ['regeringen.dk']
     start_urls = ['https://www.regeringen.dk/nyheder/']
 
     rules = (
         # Extract links matching '*\/nyheder\/*' and parse them with the spider's method parse_item
-        Rule(LinkExtractor(allow=('[a-z]+\/nyheder\/[a-z]+', )), callback='parse_item'),
+        Rule(LinkExtractor(allow=('[a-z]+\/nyheder\/[a-z]+',)), callback='parse_item'),
     )
 
     def parse_item(self, response):
         self.logger.info('This is a Danish Government news page! %s', response.url)
-        
+
         yield {
             'article_title': response.xpath('//section[@class="article-top"]/div/h1/text()').extract_first(),
             'article_date': response.xpath('//section[@class="article-top"]/div/div/time/text()').extract_first(),
